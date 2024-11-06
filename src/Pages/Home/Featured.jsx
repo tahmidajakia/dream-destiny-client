a26732c54eb578b3e972c1814dad3b3e2b6d312c
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-11-06 05:37:52 -0800

    created newslatter section

--- src/Pages/Home/Featured.jsx
+++ src/Pages/Home/Featured.jsx
@@ -9,13 +9,13 @@ const Featured = () => {
 
   return (
     <div className="font-inter max-w-7xl mx-auto px-4 md:px-6 lg:px-8 mt-16 md:mt-20 text-center md:text-left mb-20">
-      <p className="text-base md:text-lg lg:text-xl text-gray-600 lg:w-2/5 mx-auto md:mx-0">
+      <p className="text-base md:text-lg lg:text-xl text-primaryColor lg:w-2/5 mx-auto md:mx-0">
         ENJOY WORLD-CLASS STAY EXPERIENCE
       </p>
       <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primaryColor dark:text-white mt-4 md:mt-6">
         Featured Rooms
       </h1>
-      <p className="mt-4 text-sm md:text-base lg:text-lg text-gray-500 lg:w-1/2 mx-auto md:mx-0">
+      <p className="mt-4 text-sm md:text-base lg:text-lg text-primaryColor lg:w-1/2 mx-auto md:mx-0">
         Elevate your stay with our dreamy Destiny. Boasting modern design,
         expansive living spaces, and breathtaking city vistas, this penthouse
         suite offers the epitome of luxury living. Experience sophistication and
